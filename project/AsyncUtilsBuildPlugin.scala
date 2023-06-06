b8de4570492161166e2b2711e1ab9e775fe1e536
Brian Holt <bholt@dwolla.com>
2023-06-06 11:47:19 -0500

    update Scala versions to latest patch releases

--- project/AsyncUtilsBuildPlugin.scala
+++ project/AsyncUtilsBuildPlugin.scala
@@ -54,8 +54,8 @@ object AsyncUtilsBuildPlugin extends AutoPlugin {
 
   private val supportedVersions = (currentTwitterVersion :: oldVersions).sorted.reverse
 
-  private val SCALA_2_13: String = "2.13.10"
-  private val SCALA_2_12 = "2.12.17"
+  private val SCALA_2_13: String = "2.13.11"
+  private val SCALA_2_12 = "2.12.18"
   private val Scala2Versions: Seq[String] = Seq(SCALA_2_13, SCALA_2_12)
 
   private val CatsEffect3V = "3.5.0"
