ab0f8454970113b6707ccecb2ac297bc46929c06
Brian Holt <bholt@dwolla.com>
2023-06-06 12:04:55 -0500

    update scalatest references after transitive version upgrade

--- scalafix/tests/src/test/scala/com/dwolla/scrooge/scalafix/RuleSuite.scala
+++ scalafix/tests/src/test/scala/com/dwolla/scrooge/scalafix/RuleSuite.scala
@@ -1,8 +1,8 @@
 package com.dwolla.scrooge.scalafix
 
 import scalafix.testkit._
-import org.scalatest.FunSuiteLike
+import org.scalatest.funsuite.AnyFunSuiteLike
 
-class RuleSuite extends AbstractSemanticRuleSuite with FunSuiteLike {
+class RuleSuite extends AbstractSemanticRuleSuite with AnyFunSuiteLike {
   runAllTests()
 }
